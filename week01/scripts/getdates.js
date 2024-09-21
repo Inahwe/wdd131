f83435366678f120ee8d93cb77e5744e7e719fc3
Inah Willie <144800789+Inahwe@users.noreply.github.com>
2024-09-21 15:24:25 +0100

    Week01 index.html

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set the current year dynamically in the footer
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Set the last modified date dynamically in the footer
+document.getElementById('lastModified').textContent = 'Last Modified: ' + document.lastModified;
